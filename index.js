0a0ff3a1b7917cffac0e6a71a9149ddde16491f5
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-05-22 11:46:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,28 +28,38 @@ function create() {
   var dvdtileone = map.addTilesetImage('tilesetone','dvdtileone')
   var SkyOne = map.createStaticLayer("SkyOne", [dvdtileone], 0, 0)
   var GroundBlackOne = map.createStaticLayer("GroundBlackOne", [dvdtileone], 0, 0)
-  real.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-  real.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  real.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  real.hero = this.physics.add.sprite(50,50,'fireDragon')
-  this.physics.add.collider(real.hero, GroundBlackOne)
-  this.physics.add.collider(real.hero, SkyOne)
+    real.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    real.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    real.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    real.hero = this.physics.add.sprite(50,50,'fireDragon')
+this.physics.add.collider(real.hero, GroundBlackOne)
+this.physics.add.collider(real.hero, SkyOne)
   GroundBlackOne.setCollisionByProperty({collides:true}) 
   GroundBlackOne.setCollision([56,57,58,59])
   SkyOne.setCollisionByProperty({collides:true})
   real.hero.setCollideWorldBounds(true);
+  real.hero.setScale(2) 
 }
 function update() {
-  if(real.keyD.isDown) {
+  if(real.keyD.isDown) 
+  {
     real.hero.x += 5
   }
-  if(real.keyA.isDown) {
+  if(real.keyA.isDown) 
+  {
     real.hero.x -= 5
   }
-  if(real.keyW.isDown) {
+  if(real.keyW.isDown) 
+  {
     real.hero.y -= 5
   }
-   if(real.keyW.isDown && real.keyA.isDown) {
+   if(real.keyW.isDown && real.keyA.isDown) 
+   {
+    real.hero.y -= 4
+    real.hero.x -= 5
+  }
+  if(real.keyW.isDown && real.keyD.isDown) 
+  {
     real.hero.y -= 4
     real.hero.x -= 5
   }
